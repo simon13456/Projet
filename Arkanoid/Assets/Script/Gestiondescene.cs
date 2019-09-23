745bc3dc544d216a1bfdec8532f2eeef471f8c4a
Simon Leroux <simonlerou@gmail.com>
2019-09-23 18:45:17 -0400

    V1

---
+++ Arkanoid/Assets/Script/Gestiondescene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class Gestiondescene : MonoBehaviour
+{
+    public void ChangerScene()
+    {
+        int indexSceneCourante = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(indexSceneCourante+1);
+    }
+    public void quitter()
+    {
+        Application.Quit();
+    }
+    public void reco()
+    {
+        int indexSceneCourante = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(0);
+    }
+}
