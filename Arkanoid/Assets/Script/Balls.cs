c58eab7346a3c09f6b8af5090e944392ba6d64c2
Simon Leroux <simon.leroux.01@edu.cegeptr.qc.ca>
2019-09-24 10:25:08 -0400

    pares le cours du 24

---
+++ Arkanoid/Assets/Script/Balls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    private Paddle _paddle1 = new Paddle();
+    [SerializeField] float _pousseX=0.5f;
+    [SerializeField] float _pussY=5f;
+    private Vector2 _PaddleToBall;
+    private bool _estLance = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _paddle1 = FindObjectOfType<Paddle>();
+        _PaddleToBall = transform.position - _paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_estLance)
+        {
+            PositionBall();
+            lancerBall();
+        }
+    }
+
+    private void lancerBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(_pousseX, _pussY);
+            _estLance = true;
+        }
+    }
+
+    private void PositionBall()
+    {
+        Vector2 paddlePos = new Vector2(_paddle1.transform.position.x, _paddle1.transform.position.y);
+        transform.position = paddlePos + _PaddleToBall;
+    }
+}
---
+++ Arkanoid/Assets/Script/Oncollison.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oncollison : MonoBehaviour
+{
+    public
+        Niveau niveau;
+    // Start is called before the first frame update
+    void Start()
+    {
+        niveau = FindObjectOfType<Niveau>();
+        niveau.compterBriques();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        niveau.enleverBrique();
+        Destroy(gameObject, 0f);
+
+    }
+    
+}
